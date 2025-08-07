b4065b2f5a0d787592c0f81482e5dab3d6e99c88
Sujon ghumbabu <mdabrahon926@gmail.com>
2025-08-07 23:30:59 +0600

    update review model

--- b2c/reviews/models.py
+++ b2c/reviews/models.py
@@ -1,13 +1,19 @@
+from django.conf import settings
 from django.db import models
-from django.contrib.auth.models import User
-from b2c.products.models import Products 
-from common .models import TimeStampedModel
+from b2c.products.models import Products  
 
-
-# Create your models here.
-class models(TimeStampedModel):
+class Review(models.Model):
     product = models.ForeignKey(Products, on_delete=models.CASCADE, related_name='reviews')
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    rating = models.IntegerField()
-    comment = models.TextField()
+    
+    # Use the custom user model defined in settings.py
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    
+    rating = models.IntegerField()  # 1 to 5
+    comment = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        unique_together = ('product', 'user')  
+
+    def __str__(self):
+        return f"{self.user.username} - {self.product.name} - {self.rating}⭐"
