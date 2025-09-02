06ca6f469f159162308b49628d088ee6036b4aea
Sujon ghumbabu <mdabrahon926@gmail.com>
2025-09-02 23:29:56 +0600

    fixed product issue

--- b2b/product/models.py
+++ b2b/product/models.py
@@ -1,45 +1,4 @@
-# from django.db import models
-# import uuid
-# import os
-# from django.utils.text import slugify
-# from django.contrib.postgres.fields import ArrayField
-# from django.db import models
 
-
-# class Category(models.Model):
-#     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-#     name = models.CharField(max_length=255, unique=True)
-#     def __str__(self):
-#         return self.name
-
-
-# def product_image_path(instance, filename):
-#     base, ext = os.path.splitext(filename)
-#     slug = slugify(instance.title)
-#     return f'product_images/{slug}-{uuid.uuid4().hex}{ext}'
-
-
-
-
-
-# class Product(models.Model):
-#     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-#     title = models.CharField(max_length=255)
-#     product_code = models.CharField(max_length=255, unique=True, editable=False)
-#     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
-#     colors = models.JSONField(default=list, blank=True) 
-#     available_stock = models.PositiveIntegerField()
-#     price = models.DecimalField(max_digits=10, decimal_places=2)
-#     description = models.TextField()
-#     images = models.JSONField(default=list, blank=True)  
-
-#     def save(self, *args, **kwargs):
-#         if not self.product_code:
-#             self.product_code = str(uuid.uuid4()).split('-')[0]
-#         super().save(*args, **kwargs)
-
-#     def __str__(self):
-#         return self.title
 import uuid
 import os
 from django.db import models
--- b2b/product/serializers.py
+++ b2b/product/serializers.py
@@ -12,12 +12,72 @@ class CategorySerializer(serializers.ModelSerializer):
         fields = ['id', 'name']
 
 
-class ProductSerializer(serializers.ModelSerializer):
-    # Instead of PK, allow UID
-    category = serializers.PrimaryKeyRelatedField(
-        queryset=Category.objects.all()  
-    )
+# class ProductSerializer(serializers.ModelSerializer):
+#     # Instead of PK, allow UID
+#     category = serializers.PrimaryKeyRelatedField(
+#         queryset=Category.objects.all()  
+#     )
    
+
+#     images_upload = serializers.ListField(
+#         child=serializers.ImageField(),
+#         write_only=True,
+#         required=False
+#     )
+
+#     class Meta:
+#         model = Product
+#         fields = [
+#             'id', 'title', 'product_code', 'category',
+#             'colors', 'available_stock', 'price', 'description',
+#             'images', 'images_upload'
+#         ]
+#         read_only_fields = ['id', 'product_code', 'images']
+
+#     def create(self, validated_data):
+#         # Get raw colors
+#         colors = self.initial_data.get('colors', [])
+#         print("Initial colors data:", colors)
+
+#         # Parse colors
+#         if isinstance(colors, str):
+#             try:
+#                 colors = json.loads(colors)
+#             except json.JSONDecodeError:
+#                 colors = [color.strip() for color in colors.split(',') if color.strip()]
+#         if not colors:
+
+#            colors = []
+#         validated_data['colors'] = colors
+
+#         # Handle category by UID
+#         category_uid = validated_data.pop('category')
+#         try:
+#             category = Category.objects.get(id=category_uid)
+#         except Category.DoesNotExist:
+#             raise serializers.ValidationError({"category": "Invalid category UID"})
+#         validated_data['category'] = category
+
+#         # Handle images
+#         images = validated_data.pop('images_upload', [])
+#         product = Product.objects.create(**validated_data)
+
+#         image_paths = []
+#         for image in images:
+#             ext = image.name.split('.')[-1]
+#             seo_name = f"{slugify(product.title)}-{uuid.uuid4().hex}.{ext}"
+#             full_path = f'product_images/{seo_name}'
+#             saved_path = default_storage.save(full_path, image)
+#             image_paths.append(saved_path)
+
+#         product.images = image_paths
+#         product.save()
+#         return product
+class ProductSerializer(serializers.ModelSerializer):
+    category = serializers.PrimaryKeyRelatedField(
+        queryset=Category.objects.all()
+    )
+    category_detail = CategorySerializer(source='category', read_only=True)
 
     images_upload = serializers.ListField(
         child=serializers.ImageField(),
@@ -35,30 +95,22 @@ class ProductSerializer(serializers.ModelSerializer):
         read_only_fields = ['id', 'product_code', 'images']
 
     def create(self, validated_data):
-        # Get raw colors
+        # --- handle colors ---
         colors = self.initial_data.get('colors', [])
-        print("Initial colors data:", colors)
-
-        # Parse colors
         if isinstance(colors, str):
             try:
-                colors = json.loads(colors)
+                colors = json.loads(colors) 
             except json.JSONDecodeError:
-                colors = [color.strip() for color in colors.split(',') if color.strip()]
-        if not colors:
-
-           colors = []
+                colors = [c.strip() for c in colors.split(',') if c.strip()]
+        if not isinstance(colors, list):
+            colors = []
         validated_data['colors'] = colors
 
-        # Handle category by UID
-        category_uid = validated_data.pop('category')
-        try:
-            category = Category.objects.get(id=category_uid)
-        except Category.DoesNotExist:
-            raise serializers.ValidationError({"category": "Invalid category UID"})
-        validated_data['category'] = category
+        # --- handle category ---
+        category_obj = validated_data.pop('category')
+        validated_data['category'] = category_obj
 
-        # Handle images
+        # --- handle images (optional) ---
         images = validated_data.pop('images_upload', [])
         product = Product.objects.create(**validated_data)
 
